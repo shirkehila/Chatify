70cf3a2c3f8348572b122d41627272f237a506cc
Shir Kehila <shirkpro@gmail.com>
2019-03-14 15:25:30 +0200

    Change first message to 'enter username'

--- client.py
+++ client.py
@@ -36,7 +36,7 @@ top.title("Chatter")
 
 messages_frame = tkinter.Frame(top)
 my_msg = tkinter.StringVar()  # For the messages to be sent.
-my_msg.set("Type your messages here.")
+my_msg.set("Enter username")
 scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
 # Following will contain the messages.
 msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
